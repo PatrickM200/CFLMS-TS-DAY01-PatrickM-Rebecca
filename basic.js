5811fb29139b32fab0008981b076a551a82f41db
Patrick Miglar <miglar.patrick@gmail.com>
2020-06-17 11:28:38 +0200

    basic 1

---
+++ basic.js
@@ -0,0 +1,20 @@
+var table = /** @class */ (function () {
+    function table(x) {
+        for (var i = 1; i <= 10; i++) {
+            var y = x * i;
+            var div = document.createElement("div");
+            div.innerHTML = x + "*" + i + "=" + y + "\n";
+            document.body.appendChild(div);
+        }
+    }
+    return table;
+}());
+var multi = new Array();
+for (var r = 1; r < multi.length; r++) {
+    multi[r] = r;
+    div.immerHTML = r + "*" + i + "=" + y + "\n";
+    document.body.appendChild(div);
+}
+var p = new table(r);
+var p = new table(1);
+var p = new table(2);
---
+++ basic.ts
@@ -0,0 +1,22 @@
+class table {
+  x: number;
+  constructor(x: number, ) {
+    for (let i = 1; i <= 10; i++) {
+      let y = x * i;
+      let div = document.createElement("div");
+      div.innerHTML = x + "*" + i + "=" + y + "\n";
+      document.body.appendChild(div);
+    }
+  }
+}
+var multi:number[] = new Array()
+for (var r = 1; r < multi.length; r++) {
+  multi[r] = r
+  div.immerHTML = r + "*" + i + "=" + y + "\n";
+  document.body.appendChild(div);
+}
+
+let p = new table(r);
+let p = new table(1);
+let p = new table(2);
+
